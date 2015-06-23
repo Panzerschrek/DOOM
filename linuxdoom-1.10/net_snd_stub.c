ea015384bf33580eefce5c184ecb83c7972b0b61
Panzerschrek <AntiPanzerschrek@yandex.ru>
2015-06-23 22:12:33 +0700

    pilü vyvod zvuka.

--- linuxdoom-1.10/net_snd_stub.c
+++ linuxdoom-1.10/net_snd_stub.c
@@ -11,35 +11,89 @@
 
 #include "doomdef.h"
 
+
+#define MAX_CHANNELS 32
+
+typedef short sample_t;
+
+typedef struct
+{
+    int		id; // id for sound, 0 - not initialized
+
+    byte*	src_data;
+    int		length; // sample count of source data
+    int		pos; // position in destination buffer ( in destination values )
+    fixed_t	pitch;
+    fixed_t	volume;
+} snd_channel_t;
+
+static int snd_id = 1; // 0 is reserved
+
+snd_channel_t channels[MAX_CHANNELS];
+
 struct
 {
     SDL_AudioSpec format;
     int device_id;
 } sdl_audio;
 
-void SDLCALL * AudioCallback (void *userdata, Uint8 * stream, int len)
+
+static int nearestpotfloor(int x)
 {
+    int r = 1<<30;
+    while(r > x) r>>= 1;
+    return r;
+}
+
+void SDLCALL AudioCallback (void *userdata, Uint8 * stream, int len)
+{
+    int		i;
+    sample_t*	data;
+    static 	int shift = 0;
+
+    data = (sample_t*) stream;
+
+    static sfxinfo_t info;
+    static int length;
+    if (!info.name)
+    {
+	info.name = malloc( 32 );
+	strcpy(info.name, "getpow");
+	info.lumpnum = I_GetSfxLumpNum(&info);
+
+	info.data = (char *) W_CacheLumpNum(info.lumpnum, PU_MUSIC) + sizeof(lumpinfo_t);
+
+	length = W_LumpLength(info.lumpnum);
+    }
+
+    for( i= 0; i < len / sizeof(sample_t) && shift < length; i++ )
+    {
+    	data[i] = ((char*)info.data)[shift] << 7;
+    	shift++;
+    }
+    for( ; i < len / sizeof(sample_t); i++ )
+	data[i] = 0;
 }
 
 void I_InitSound()
 {
     SDL_AudioSpec	audio_format;
-    char*		device_name;
+    const char*		device_name;
     int			i;
     int			num_devices;
-    int			device_id;
 
     SDL_InitSubSystem( SDL_INIT_AUDIO );
 
 
     audio_format.channels = 1;
-    audio_format.freq = 22050;
+    audio_format.freq = 11025;
     audio_format.format = AUDIO_S16;
     audio_format.silence = 0;
-    audio_format.samples = 16384;
     audio_format.callback = AudioCallback;
 
-    device_id = 0;
+    audio_format.samples = nearestpotfloor(audio_format.freq / TICRATE);
+
+    sdl_audio.device_id = 0;
     num_devices = SDL_GetNumAudioDevices(0);
     for( i = 0; i < num_devices; i++ )
     {
@@ -57,6 +111,18 @@ void I_InitSound()
 	printf( "I_InitSound: Could not open audio device\n" );
 	return;
     }
+
+    for( i = 0; i < MAX_CHANNELS; i++ )
+    {
+    	channels[i].id = 0;
+    }
+
+    SDL_PauseAudioDevice(sdl_audio.device_id , 0);
+}
+
+void I_UpdateSound()
+{
+
 }
 
 void I_ShutdownSound()
@@ -79,6 +145,38 @@ int I_StartSound
   int		pitch,
   int		priority )
 {
+    int			i;
+    int			freeslot;
+    snd_channel_t*	channel;
+    sfxinfo_t*		info;
+    char name[16];
+
+    freeslot = -1;
+    for( i = 0; i < MAX_CHANNELS; i++ )
+    	if ( channels[i].id == 0 )
+    	{
+	    freeslot = i;
+	    break;
+    	}
+
+    if( freeslot == -1 ) return 0;
+
+    channel = &channels[ freeslot ];
+
+    channel->id = snd_id;
+    snd_id++;
+
+    channel->pos = 0;
+    channel->pitch = 1 << FRACBITS;
+    channel->volume = (vol << FRACBITS) / 128;
+
+    info = &S_sfx[id];
+    info->data = W_CacheLumpNum(info->lumpnum, PU_SOUND) + sizeof(lumpinfo_t);
+
+    channel->src_data = info->data;
+    //TODO
+
+    return channel->id;
 }
 
 int I_SoundIsPlaying(int handle)
@@ -154,6 +252,13 @@ I_UpdateSoundParams
 {
 }
 
-int I_GetSfxLumpNum (sfxinfo_t* sfxinfo )
+//
+// Retrieve the raw data lump index
+//  for a given SFX name.
+//
+int I_GetSfxLumpNum(sfxinfo_t* sfx)
 {
+    char namebuf[9];
+    sprintf(namebuf, "ds%s", sfx->name);
+    return W_GetNumForName(namebuf);
 }
