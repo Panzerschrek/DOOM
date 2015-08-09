a4be812d1584a1567ae4f606a02c5877111150ad
Panzerschrek <AntiPanzerschrek@yandex.ru>
2015-08-09 14:43:03 +0700

    Sdelal funkçiju tumana v 8-bitnom renderere nezavisimoj ot razmerov ekrana.

--- linuxdoom-1.10/r_main.c
+++ linuxdoom-1.10/r_main.c
@@ -627,7 +627,7 @@ void R_InitLightTables (void)
 	startmap = ((LIGHTLEVELS-1-i)*2)*NUMCOLORMAPS/LIGHTLEVELS;
 	for (j=0 ; j<MAXLIGHTZ ; j++)
 	{
-	    scale = FixedDiv ((SCREENWIDTH/2*FRACUNIT), (j+1)<<LIGHTZSHIFT);
+	    scale = FixedDiv ((ID_SCREENWIDTH/2*FRACUNIT), (j+1)<<LIGHTZSHIFT);
 	    scale >>= LIGHTSCALESHIFT;
 	    level = startmap - scale/DISTMAP;
 
